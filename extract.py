69aad72166e4258ce600dd9867f0f84305901607
raman77768 <53868914+raman77768@users.noreply.github.com>
2020-12-10 00:28:53 +0530

    video links and thumbnails

--- extract.py
+++ extract.py
@@ -7,10 +7,10 @@ import pprint
 def extract_videos(search_keyword):
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    #print("https://www.youtube.com/watch?v=" + video_ids[0])
+    return "https://www.youtube.com/watch?v=" + video_ids[0],video_ids[0]
 
-    VideoID = video_ids[0]
 
+def extract_img(VideoID):
     params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % VideoID}
     url = "https://www.youtube.com/oembed"
     query_string = urllib.parse.urlencode(params)
@@ -20,5 +20,5 @@ def extract_videos(search_keyword):
     response_text = response.read()
     data = json.loads(response_text.decode())
     #pprint.pprint(data)
-    return data['title']
+    return data['thumbnail_url']
 
