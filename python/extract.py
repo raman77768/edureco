7f65cdccc852cc62ba54c8df584f998bd661cb30
raman77768 <53868914+raman77768@users.noreply.github.com>
2020-12-10 21:35:34 +0530

    final software

--- python/extract.py
+++ python/extract.py
@@ -5,6 +5,7 @@ import urllib
 import pprint
 
 def extract_videos(search_keyword):
+    search_keyword = search_keyword.strip().replace(' ','+')
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
     return "https://www.youtube.com/watch?v=" + video_ids[0],video_ids[0]
